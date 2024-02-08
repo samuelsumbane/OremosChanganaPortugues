520138d25de6e96d32f0ec041b721996da41c14a
samuelsumbane <samuelsumbane143@gmail.com>
2024-02-08 02:30:31 +0200

    app icon e app name

--- app/src/main/java/com/samuel/oremoschangana/view/favoritos.kt
+++ app/src/main/java/com/samuel/oremoschangana/view/favoritos.kt
@@ -1,10 +1,10 @@
 package com.samuel.oremoschangana.view
 
+//import androidx.compose.material3.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,42 +15,30 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-
-import androidx.compose.material3.*
-
 import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.samuel.oremoschangana.ui.theme.White
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.samuel.oremoschangana.apresentacaoOracao.CancaoEvent
 import com.samuel.oremoschangana.apresentacaoOracao.CancaoState
 import com.samuel.oremoschangana.apresentacaoOracao.OracaoState
@@ -59,8 +47,6 @@ import com.samuel.oremoschangana.components.BottomAppBarPrincipal
 import com.samuel.oremoschangana.components.InputPesquisa
 import com.samuel.oremoschangana.functionsKotlin.isNumber
 import com.samuel.oremoschangana.ui.theme.Orange
-import com.samuel.oremoschangana.ui.theme.Shapes
-import com.samuel.oremoschangana.components.InputPesquisa as InputPesquisa1
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -109,7 +95,7 @@ fun FavoritosPage(state: OracaoState, cstate: CancaoState, navController: NavCon
                 Column(
                     modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
                 ){
-                    Text(text = "Nenhuma oração ou cântico encontrado.", textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
+                    Text(text = "Nenhumo cântico encontrado.", textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
                 }
             }else if (fCancoes.isNotEmpty()) {
 
--- app/src/main/java/com/samuel/oremoschangana/view/oracoes.kt
+++ app/src/main/java/com/samuel/oremoschangana/view/oracoes.kt
@@ -2,55 +2,44 @@ package com.samuel.oremoschangana.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-
-import androidx.compose.material3.*
-
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import com.samuel.oremoschangana.components.BottomAppBarPrincipal
-import com.samuel.oremoschangana.components.InputPesquisa
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.samuel.oremoschangana.apresentacaoOracao.CancaoEvent
+import androidx.navigation.NavController
 import com.samuel.oremoschangana.apresentacaoOracao.OracaoState
 import com.samuel.oremoschangana.apresentacaoOracao.OracoesEvent
-import com.samuel.oremoschangana.dataOracao.Oracao
-import com.samuel.oremoschangana.ui.theme.Orange
-import com.samuel.oremoschangana.ui.theme.White
+import com.samuel.oremoschangana.components.BottomAppBarPrincipal
+import com.samuel.oremoschangana.components.InputPesquisa
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -147,26 +136,28 @@ fun OracoesPage(state: OracaoState, navController: NavController, onEvent: (Orac
                             }
                         }
 
-                        IconButton(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(0.1f)
-                                .height(60.dp),
-                            onClick = {
-                                val status = if (oracao.favorito){
-                                    false
-                                }else{
-                                    true
-                                }
-                                onEvent(OracoesEvent.UpdateFavorito(oracaoId = oracao.id, novoFavorito = status))
-                            }
-                        ){
-                            if (oracao.favorito){
-                                Icon(imageVector = Icons.Default.Star, contentDescription = "É favorito", tint = Orange)
-                            }else{
-                                Icon(imageVector = Icons.Outlined.Star, contentDescription = "Não é favorito", tint = White)
-                            }
-                        }
+                        // por falta de tempo nao sera mostrado as oracoes favoritas.
+
+//                        IconButton(
+//                            modifier = Modifier
+//                                .fillMaxSize()
+//                                .weight(0.1f)
+//                                .height(60.dp),
+//                            onClick = {
+//                                val status = if (oracao.favorito){
+//                                    false
+//                                }else{
+//                                    true
+//                                }
+//                                onEvent(OracoesEvent.UpdateFavorito(oracaoId = oracao.id, novoFavorito = status))
+//                            }
+//                        ){
+//                            if (oracao.favorito){
+//                                Icon(imageVector = Icons.Default.Star, contentDescription = "É favorito", tint = Orange)
+//                            }else{
+//                                Icon(imageVector = Icons.Outlined.Star, contentDescription = "Não é favorito", tint = White)
+//                            }
+//                        }
 
                     }
                 }
